04179a06146d0003dc69bf34ebd4d4daf41d589a
Nhattu2604 <nguyennhattucr01ld@gmail.com>
2025-02-20 01:07:10 +0700

    ví dụ về arraylist
    khai báo arraylist và nhập 3 sản phẩm sau đó xuất ra màn hình

--- viduhuongdoituong/src/CT.java
+++ viduhuongdoituong/src/CT.java
@@ -1,16 +1,29 @@
+import java.util.ArrayList;
 
 public class CT {
  public static void main(String[] args) {
-	 SanPham sp1 = new SanPham();
-	 SanPham sp2 = new SanPham(2,"Bánh Chuối","Đồ ăn nhanh","bc.jpg");
-	 sp1.setMaSP(1);
-	 sp1.setTenSP("Bia SG");
-	 sp1.setAnhSP("sg.png");
-	 String thongtinSP1 = sp1.toString();
-	 String thongtinSP2 = "Mã sản phẩm là " + sp2.getMaSP();
-	 		thongtinSP2 +=" Tên SP2 là " + sp2.getTenSP();
-	 System.out.println(thongtinSP1);
-	 System.out.println(thongtinSP2);
+	// khai báo danh sách sản phẩm 
+	 ArrayList<SanPham> dsSanPham;
+	 // Xin Mới
+	 dsSanPham = new ArrayList<SanPham>();
+	 // nhập 3 sản phẩm 
+	 SanPham sp1 = new SanPham(1,"Bánh Chuối","Đồ ăn nhanh","bc1.jpg");
+	 SanPham sp2 = new SanPham(2,"Bánh Quy","Đồ ăn nhanh","bc2.jpg");
+	 SanPham sp3 = new SanPham(3,"Bánh Pizza","Đồ ăn nhanh","bc3.jpg");
+	 dsSanPham.add(sp1);
+	 dsSanPham.add(sp2);
+	 dsSanPham.add(sp3);
+	 
+//	 for (SanPham x: dsSanPham)
+//		 System.out.println(x.toString());
+	 
+	 for (int i = 0; i<dsSanPham.size(); i++) {
+		
+		 
+			SanPham sp = dsSanPham.get(i);
+			System.out.println(dsSanPham.get(i));
+			 
+	 }
  }
  
 }
