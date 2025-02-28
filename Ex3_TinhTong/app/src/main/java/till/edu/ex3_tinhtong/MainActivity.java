328373fddabd547cb2a169dbe15bc1e11a9da2ce
Nhattu2604 <nguyennhattucr01ld@gmail.com>
2025-02-28 15:01:50 +0700

    Ex3.2_TinhTong
    Xử lý backend: Bắt/Đáp ứng sự kiện bằng hàm xử lý cài đặt cho OnClick trên XML

--- Ex3_TinhTong/app/src/main/java/till/edu/ex3_tinhtong/MainActivity.java
+++ Ex3_TinhTong/app/src/main/java/till/edu/ex3_tinhtong/MainActivity.java
@@ -1,7 +1,8 @@
 package till.edu.ex3_tinhtong;
 
 import android.os.Bundle;
-
+import android.widget.EditText;
+import android.view.View;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -20,5 +21,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+  // Đây là bô lắng nghẹ và xử lý sự kiện click lên nút tính tổng
+    public void XuLyCong(View view){
+        //Tìm tham chiếu đến điều khiên trên tệp XML,mapping sang java file
+      EditText editTextSoA = findViewById(R.id.edtA);
+      EditText editTextSoB = findViewById(R.id.edtB);
+      EditText editTextketQua = findViewById(R.id.edtKQ);
+
+      // lay du lieu ve o dieu khien so A
+      String strA =  editTextSoA.getText().toString(); //strA ="2"
+      // lay du lieu ve o dieu khien so B
+        String strB =  editTextSoB.getText().toString(); //strB ="4"
+      // chuyen du lieu sang dang so
+       int so_A = Integer.parseInt(strA);
+       int so_B = Integer.parseInt(strB);
+
+       // Tinh toan theo yeu cau
+        int tong = so_A + so_B;     //6
+        String strTong = String.valueOf(tong); // chuyen sang dang chuoi "6"
+        // hien ra man hinh
+        editTextketQua.setText(strTong);
+
+
     }
 }
