37866513f6f51d0ab0d08d206117137791cb802f
Nhattu2604 <nguyennhattucr01ld@gmail.com>
2025-03-11 00:12:34 +0700

    nằm trong vd2.1 nên tách ra làm lại
    sửa lại vì nằm trong file vd 2.1

--- Ex6_IntentDonGian/app/src/main/java/till/edu/ex6_intentdongian/MainActivity.java
+++ Ex6_IntentDonGian/app/src/main/java/till/edu/ex6_intentdongian/MainActivity.java
@@ -1,6 +1,9 @@
 package till.edu.ex6_intentdongian;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button nutMH2;
+    Button nutMH3;
+    void TimDieuKhien(){
+        nutMH2 = (Button) findViewById(R.id.btnMH2);
+        nutMH3 = (Button) findViewById(R.id.btnMH3);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        TimDieuKhien();
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH2 = new Intent(MainActivity.this,MH2Activity.class);
+                startActivity(intentMH2);
+            }
+
+        });
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH3 = new Intent(MainActivity.this,MH3Activity.class);
+                startActivity(intentMH3);
+            }
         });
     }
 }
