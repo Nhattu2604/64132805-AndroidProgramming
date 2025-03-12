6b754d029aff2f15ac35c3629dfb46f0e800391b
Nhattu2604 <nguyennhattucr01ld@gmail.com>
2025-03-13 00:10:57 +0700

    List View NNLT

--- LVNgonNguLapTrinh/app/src/main/java/till/edu/lvngonngulaptrinh/MainActivity.java
+++ LVNgonNguLapTrinh/app/src/main/java/till/edu/lvngonngulaptrinh/MainActivity.java
@@ -1,7 +1,11 @@
 package till.edu.lvngonngulaptrinh;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,6 +34,22 @@ public class MainActivity extends AppCompatActivity {
         dsNgonNguLT = new ArrayList<String>();
         dsNgonNguLT.add("Python");
         dsNgonNguLT.add("Php");
+        dsNgonNguLT.add("Java");
         //B2:
+        ArrayAdapter<String> addapterNNLT;
+        addapterNNLT = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,dsNgonNguLT);
+        //b3 gan addapter
+        ListViewNNLT.setAdapter(addapterNNLT);
+        //b4 gan bo lang nghe va sly su kien
+        ListViewNNLT.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // bien position da chua vi tri cua item duoc click
+                String giatriduocchon = dsNgonNguLT.get(position);
+                // lam theo yeu cau bat ky doi voi gia tri get duoc
+                // vd toast len
+                Toast.makeText(MainActivity.this,giatriduocchon,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
