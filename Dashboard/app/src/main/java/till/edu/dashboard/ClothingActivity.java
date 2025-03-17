8c57709f4bcf36770f3eb0d9b20f00ace875ed4a
Nhattu2604 <nguyennhattucr01ld@gmail.com>
2025-03-17 23:49:47 +0700

    Dashboard

--- Dashboard/app/src/main/java/till/edu/dashboard/ClothingActivity.java
+++ Dashboard/app/src/main/java/till/edu/dashboard/ClothingActivity.java
@@ -1,41 +1,24 @@
 package till.edu.dashboard;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    CardView clothingCard;
+public class ClothingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        // Set padding to avoid system UI overlaps
+        setContentView(R.layout.activity_clothing);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        // Move this line outside of the window insets listener
-        clothingCard = findViewById(R.id.clothingCard);
-
-        clothingCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ClothingActivity.class);
-                startActivity(intent);
-            }
         });
     }
 }
